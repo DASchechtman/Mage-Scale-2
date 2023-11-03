3cc454b4acd5b7b0dc85e6c28f86ec1d54c4ff93
Daniel Schechtman <dschechtman@carthage.edu>
2023-11-02 23:07:14 -0500

    created my first export to itch

---
+++ Icon.gd
@@ -0,0 +1,26 @@
+extends Sprite2D
+
+var step: int = 0
+var switch: bool = true
+const RANGE := 100
+const INC := 400
+
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float):
+	if step == RANGE:
+		switch = false
+	elif step == RANGE * -1:
+		switch = true
+	
+	if switch:
+		self.position.x += (INC * delta)
+		step += 1
+	else:
+		self.position.x -= (INC * delta)
+		step -= 1
+	
+
