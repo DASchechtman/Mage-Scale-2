ba68f992f4919ce2f3af7240afa319b155bc47cb
Daniel Schechtman <dschechtman@carthage.edu>
2023-11-02 21:44:51 -0500

    first git commit with setup complete

--- Node2D.gd
+++ Node2D.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
+var secs: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var x := 1
+	while x < 10:
+		print(x)
+		x += 1
 	print("hello world")
+
+func _process(delta: float):
+	if secs >= 5:
+		secs = 0
+		print("1 minute has passed")
+	else:
+		secs += delta
