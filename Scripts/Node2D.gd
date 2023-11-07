34d9577dfa238ce3c6ac8086f0018486ac187561
Daniel Schechtman <dschechtman@carthage.edu>
2023-11-06 20:04:04 -0600

    started learning how to dynamically load assets into the scene with godot

--- Scripts/Node2D.gd
+++ Scripts/Node2D.gd
@@ -1,28 +1,9 @@
 extends Node2D
 
 var secs: float = 0
+var x: Callable = func (y: int) -> int:
+	return y + 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var x = {}
-	var _y: Array[String] = []
-	x["4"] = "hello"
-	print(x["4"])
-	print(_y)
-
-func _process(delta: float):
-	_PrintAfter(5, delta, "5 seconds have passed")
-
-func _StrArr(x: Array[int]) -> Array[String]:
-	var y: Array[String] = []
-	for i in x:
-		y.append(str(i))
-	return y
-	
-
-func _PrintAfter(seconds: int, delta: float, message: String):
-	if secs >= seconds:
-		secs = delta
-		print(message)
-	else:
-		secs += delta
+	pass
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,45 @@
 extends Sprite2D
 
 
+var x_speed: float = 0
+var y_speed: float = 0
+
+@onready var _projectile := preload("res://Prefabs/earth.tscn").instantiate()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
+	_projectile.position = Vector2.ZERO
+	get_parent().add_child(_projectile)
 	pass # Replace with function body.
+
+func _init():
+	pass
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("ui_right") or event.is_action_released("ui_right"):
+		x_speed = ___SetSpeed("ui_right", event)
+	
+	if event.is_action_pressed("ui_left") or event.is_action_released("ui_left"):
+		x_speed = -___SetSpeed("ui_left", event)
+	
+	if event.is_action_pressed("ui_up") or event.is_action_released("ui_up"):
+		y_speed = -___SetSpeed("ui_up", event)
+	
+	if event.is_action_pressed("ui_down") or event.is_action_released("ui_down"):
+		y_speed = ___SetSpeed("ui_down", event)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	self.position.x += x_speed
+	self.position.y += y_speed
+
+
+func ___SetSpeed(dir: String, event: InputEvent):
+	if event.is_action_pressed(dir):
+		return 1
+	elif event.is_action_released(dir):
+		return 0
+	return 0
