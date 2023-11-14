f2d05bc9f477d017886cda286fde8b671e677448
Daniel Schechtman <dschechtman@carthage.edu>
2023-11-14 01:37:31 -0600

    figured out how to run code based on a collision, but make the collision non-solid aka I figured out how to make a trigger at specific points

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,20 +1,29 @@
-extends Node2D
+extends CharacterBody2D
 
 var __CAMERA: Camera2D
+var __AREA: Area2D
 
 func _ready():
 	__CAMERA = $Camera2D
+	__AREA = $Area2D
 	__CAMERA.make_current()
+	__AREA.connect("body_entered", _on_body_entered)
 
 func _process(_delta):
+	var dir = null
 	if Input.is_action_pressed("ui_up"):
-		position.y -= 1
+		dir = Vector2(0, -1)
 	if Input.is_action_pressed("ui_down"):
-		position.y += 1
+		dir = Vector2(0, 1)
 	if Input.is_action_pressed("ui_left"):
-		position.x -= 1
+		dir = Vector2(-1, 0)
 	if Input.is_action_pressed("ui_right"):
-		position.x += 1
+		dir = Vector2(1, 0)
+	
+	if dir == null:
+		return
+	
+	move_and_collide(dir)
 
 func __CheckInput(event: InputEvent, dir: String, default: float):
 	if event.is_action_pressed(dir):
@@ -23,3 +32,5 @@ func __CheckInput(event: InputEvent, dir: String, default: float):
 		return 0
 	return default
 
+func _on_body_entered(_body: Node):
+	print("%s hit" % _body.to_string())
