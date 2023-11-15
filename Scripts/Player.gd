58a5395c78154a56978567d7243e1d13b013025a
Daniel Schechtman <dschechtman@carthage.edu>
2023-11-14 18:18:37 -0600

    created a way to let sound switch when certain boundries are crossed.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,12 +2,16 @@ extends CharacterBody2D
 
 var __CAMERA: Camera2D
 var __AREA: Area2D
+var __AUDIO: AudioStreamPlayer
 
 func _ready():
 	__CAMERA = $Camera2D
 	__AREA = $Area2D
+	__AUDIO = $AudioStreamPlayer
 	__CAMERA.make_current()
 	__AREA.connect("body_entered", _on_body_entered)
+	__AUDIO.connect("finished", _loop_audio)
+	
 
 func _process(_delta):
 	var dir = null
@@ -32,5 +36,22 @@ func __CheckInput(event: InputEvent, dir: String, default: float):
 		return 0
 	return default
 
+func _loop_audio():
+	__AUDIO.play()
+
 func _on_body_entered(_body: Node):
-	print("%s hit" % _body.to_string())
+	var url: String = ""
+	
+	match _body.name:
+		"Grass Area":
+			url = "res://Assets/audio/1_-_sample.mp3"
+		"Water Area":
+			url = "res://Assets/audio/2_-_sample.mp3"
+		"Sky Area":
+			url = "res://Assets/audio/3_-_sample.mp3"
+	
+	if url == "": return
+	
+	__AUDIO.stop()
+	__AUDIO.stream = load(url)
+	__AUDIO.play()
